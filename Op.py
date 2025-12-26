92ed7beb9e77b29ca804f925ea7eb8b160e9de34
ShubhamShrivastav0702 <you@example.com>
2025-12-26 11:46:45 +0530

    Python Operator

---
+++ Op.py
@@ -0,0 +1,145 @@
+
+"""
+a=9
+b=9
+
+print(a<b)
+"""
+"""
+a=10
+b=9
+print(a<=b)
+"""
+
+"""
+a=9
+b=9
+print(a>b)
+"""
+
+"""
+a=9
+b=9
+print(a>=b)
+"""
+
+"""
+
+a=9
+b=9
+
+print(a==b)
+print(a!=b)
+"""
+
+
+
+
+
+
+"""
+and  when both condition are match
+    
+     True  and True   -> True
+     True  and False  -> False
+     False and True   -> False 
+     False and False  -> False
+
+
+
+    
+
+or   if any condition are True 
+
+     True  or True   -> True
+     True  or False  -> True
+     False or True   -> True 
+     False or False  -> False
+
+not
+      not(True  and True)
+
+      not is use to check logical condition
+      and return boolean result based on relational result
+
+      not(True)  -> False
+      not(False) -> True
+"""
+
+
+
+
+"""
+member ship
+        in
+        not in
+
+        "shubham"  in  ["raj","amit","prince","virendra","abhishek"
+        False
+
+       "prince"  in  ["raj","amit","prince","virendra","abhishek"]
+        True
+
+
+     "shubham"  not in  ["raj","amit","prince","virendra","abhishek"]
+      True
+     "prince"  not in  ["raj","amit","prince","virendra","abhishek"]
+      Fasle
+
+        
+"""
+
+
+"""
+
+
+
+name="shubham"
+names=["raj","amit","prince","virendra","abhishek"]
+print(name in names)
+
+name="prince"
+names=["raj","amit","prince","virendra","abhishek"]
+print(name in names)
+
+name="shubham"
+names=["raj","amit","prince","virendra","abhishek"]
+print(name not in names)
+
+name="prince"
+names=["raj","amit","prince","virendra","abhishek"]
+print(name not in names)
+
+"""
+
+
+
+"""
+
+
+
+
+name="rahul"
+name1="raj"
+
+print( name == name1)
+print(id(name))
+print(id(name1))
+
+
+is
+
+is not
+
+
+
+
+"""
+
+
+
+name1="raj"
+
+name2="raj"
+print(name1 is name2)
+print(name1 is not name2)
